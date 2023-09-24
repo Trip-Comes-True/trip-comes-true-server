2adee38de460e4fbe4a6ae68edca522eb55836c6
unknown <dudqk9696@naver.com>
2023-09-24 21:15:20 +0900

    [Feat] : 스프링 서버 구축

---
+++ src/main/java/kr/co/tripct/tripctserver/TripComesTrueServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tripct.tripctserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripComesTrueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripComesTrueServerApplication.class, args);
+	}
+
+}
