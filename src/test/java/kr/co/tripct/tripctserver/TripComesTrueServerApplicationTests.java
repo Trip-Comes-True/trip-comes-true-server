750086d3ebc3c3a1001f46581eefb562dc655861
unknown <dudqk9696@naver.com>
2023-09-24 21:15:20 +0900

    [Feat] : 스프링 서버 구축

---
+++ src/test/java/kr/co/tripct/tripctserver/TripComesTrueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tripct.tripctserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripComesTrueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
