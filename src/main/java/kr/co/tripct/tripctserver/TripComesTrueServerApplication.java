e70faf8d34ee0e7e5846cbd783da588cff33280c
elyudwo <dudqk9696@naver.com>
2023-10-16 00:15:39 +0900

    [Fix] : git ignore 파일 수정 및 CI/CD fix

--- src/main/java/kr/co/tripct/tripctserver/TripComesTrueServerApplication.java
+++ src/main/java/kr/co/tripct/tripctserver/TripComesTrueServerApplication.java
@@ -2,8 +2,10 @@ package kr.co.tripct.tripctserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TripComesTrueServerApplication {
 
 	public static void main(String[] args) {
