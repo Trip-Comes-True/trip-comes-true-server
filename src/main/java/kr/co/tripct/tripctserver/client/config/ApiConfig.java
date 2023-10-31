0ad6d988f12070041bcdfa5d8cfde2d92b90b3a5
wellbeing-dough <kdw990202@naver.com>
2023-10-31 16:24:18 +0900

    feign 연동

---
+++ src/main/java/kr/co/tripct/tripctserver/client/config/ApiConfig.java
@@ -0,0 +1,10 @@
+package kr.co.tripct.tripctserver.client.config;
+
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class ApiConfig {
+}
