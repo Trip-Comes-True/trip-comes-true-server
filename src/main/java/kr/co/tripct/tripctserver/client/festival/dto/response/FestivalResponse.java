fde48a3a756f5e8d3a4cdc60db8d016df1e3583e
wellbeing-dough <kdw990202@naver.com>
2023-11-08 01:56:30 +0900

    불필요한 어노테이션 제거

--- src/main/java/kr/co/tripct/tripctserver/client/festival/dto/response/FestivalResponse.java
+++ src/main/java/kr/co/tripct/tripctserver/client/festival/dto/response/FestivalResponse.java
@@ -1,6 +1,5 @@
 package kr.co.tripct.tripctserver.client.festival.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.co.tripct.tripctserver.client.festival.dto.data.FestivalData;
 import lombok.Data;
 
@@ -8,15 +7,10 @@ import java.util.List;
 
 @Data
 public class FestivalResponse {
-    @JsonProperty("item")
     private List<FestivalData> item;
-    @JsonProperty("totalCnt")
     private int totalCnt;
-    @JsonProperty("resultCode")
     private String resultCode;
-    @JsonProperty("errorMsg")
     private String errorMsg;
-    @JsonProperty("resultMsg")
     private String resultMsg;
 
 }
